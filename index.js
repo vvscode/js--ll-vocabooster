ff9a7a7f66fcf3ab8ed2a0e26a0b5e5473d6d439
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-02-08 16:19:23 +0300

    Add some static files (#2)

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-(() => console.log(1))();
+const express = require("express");
+const path = require("path");
+
+const PORT = 3000;
+
+const app = express();
+
+app.get("/api/hola", (req, res) =>
+  res.send({
+    message: "Hola from BE"
+  })
+);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const server = app.listen(PORT, () =>
+  console.log(`http://0.0.0.0:${server.address().port}`)
+);
